73622724c495943be84ab3851008ebddfe371fe4
KyleMcMaster <11415127+KyleMcMaster@users.noreply.github.com>
2024-10-17 21:27:21 -0400

    Rename NewProject to NitroSynth

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -13,12 +13,12 @@
 //==============================================================================
 /**
 */
-class NewProjectAudioProcessor  : public juce::AudioProcessor
+class NitroSynthAudioProcessor  : public juce::AudioProcessor
 {
 public:
     //==============================================================================
-    NewProjectAudioProcessor();
-    ~NewProjectAudioProcessor() override;
+    NitroSynthAudioProcessor();
+    ~NitroSynthAudioProcessor() override;
 
     //==============================================================================
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
@@ -55,5 +55,5 @@ public:
 
 private:
     //==============================================================================
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NitroSynthAudioProcessor)
 };
